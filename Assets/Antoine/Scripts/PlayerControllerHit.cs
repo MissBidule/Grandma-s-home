fa6c97c4cd497fcf325068e56de00a01a7644036
Antoine Vuillet <121035207+Antoine-Vuillet@users.noreply.github.com>
2026-01-23 16:50:53 +0100

    base fighting system

--- Assets/Antoine/Scripts/PlayerControllerHit.cs
+++ Assets/Antoine/Scripts/PlayerControllerHit.cs
@@ -1,19 +1,65 @@
 using UnityEngine;
 
+
+/*
+@brief       Containts class declaration for PlayerController
+@details     The PlayerController class handles player actions by reading input from the PlayerInputController component.
+*/
 public class PlayerControllerHit : MonoBehaviour
 {
-    private PlayerInputControllerHit m_playerInputController;
+    private PlayerInputControllerHit m_playerInputControllerHit;
+
+    private BoxCollider m_boxCollider;
+    private float m_hitDuration = 0.5f;
+    private float m_hitTimer = 0f;
+
 
     private void Awake()
     {
-        m_playerInputController = GetComponent<PlayerInputControllerHit>();
+        m_playerInputControllerHit = GetComponent<PlayerInputControllerHit>();
+        m_boxCollider = GetComponent<BoxCollider>();
     }
 
+    /*
+    @brief Update triggers every frame
+    Gets the movement input vector from the PlayerInputController and moves the player accordingly.
+    @return void
+    */
     private void Update()
     {
-        Vector2 movement = m_playerInputController.m_movementInputVector;
+        Vector2 movement = m_playerInputControllerHit.m_movementInputVector;
         Vector3 moveDirection = new Vector3(movement.x, 0, movement.y);
         transform.Translate(moveDirection * Time.deltaTime * 5f);
+        if (m_playerInputControllerHit.m_isHitting)
+        {
+            EnableAttack();
+        }
+        else
+        {
+            DisableAttack();
+        }
+    }
 
+    private void EnableAttack()
+    {
+        m_boxCollider.enabled = true;
+    }
+
+    private void DisableAttack()
+    {
+        m_boxCollider.enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var ghost = other.GetComponent<PlayerGhost>();
+        if (ghost != null)
+        {
+            HitOpponent();
+        }
+    }
+    private void HitOpponent()
+    {
+        print("tape un fantôme");
     }
 }
--- Assets/Antoine/Scripts/PlayerInputControllerHit.cs
+++ Assets/Antoine/Scripts/PlayerInputControllerHit.cs
@@ -1,13 +1,22 @@
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+/*
+ * @brief       Contains class declaration for PlayerInputController
+ * @details     The PlayerInputController class handles player input using Unity's Input System.
+ */
 public class PlayerInputControllerHit : MonoBehaviour
 {
 
-    public Vector2 m_movementInputVector { get; private set;}
+    public Vector2 m_movementInputVector { get; private set; }
+
     public bool m_isHitting { get; private set; }
 
+    /*
+     * @brief OnMove is called by the Input System when movement input is detected
+     * @param _context: The context of the input action
+     * @return void
+     */
     public void OnMove(InputAction.CallbackContext _context)
     {
         m_movementInputVector = _context.ReadValue<Vector2>();
