6593f1199dd13de4a7c9eb988d24ac1965704aa8
VillerotJustin <62113284+VillerotJustin@users.noreply.github.com>
2026-02-12 17:46:08 +0100

    Creation of a custom Lobby and Networked scene to work on the lobby info transmision
    Modified WaitForPlayerState.cs to wait indefinitely if -1 player and to have the Connection starter transmit the info from the LobbyDataHolder.cs
    
    LobbyManager.cs is updating the number of player on lobby update.
    
    Code isn't using our naming convension because it's the Plugin Code that I modified I'll do that later when thing are more implemented.

--- Assets/Justin/Scripts/States/WaitForPlayerState.cs
+++ Assets/Justin/Scripts/States/WaitForPlayerState.cs
@@ -1,11 +1,18 @@
 using System.Collections;
+using PurrNet.Logging;
 using PurrNet.StateMachine;
 using UnityEngine;
 
 public class WaitForPlayerState : StateNode
 {
     // Need to be updated to wait for the number of player in the lobby
-    [SerializeField] private int m_minPlayers = 2;
+    [SerializeField] private int m_minPlayers = -1;
+
+    public void set_numPlayers(int numPlayers)
+    {
+        PurrLogger.Log("Number of players in lobby: " + numPlayers);
+        m_minPlayers = numPlayers;
+    }
     
     public override void Enter(bool _asServer)
     {
@@ -19,6 +26,9 @@ public class WaitForPlayerState : StateNode
 
     private IEnumerator WaitForPlayers()
     {
+        if (m_minPlayers == -1)
+            yield return null;
+        
         while (networkManager?.playerCount < m_minPlayers)
             yield return null;
         
