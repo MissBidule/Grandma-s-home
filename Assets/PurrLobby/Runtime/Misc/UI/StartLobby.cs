969fb131af9b52d79549d077a372e35eab1cded1
Lilou Alidor <lilou.alidor@gmail.com>
2026-02-19 11:37:15 +0100

    Better lobby name setter

---
+++ Assets/PurrLobby/Runtime/Misc/UI/StartLobby.cs
@@ -0,0 +1,20 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PurrLobby
+{
+    public class StartLobby : MonoBehaviour
+    {
+        [SerializeField] private TMP_InputField m_lobbyName;
+        [SerializeField] private LobbyManager m_lobbyManager;
+        [SerializeField] private ViewManager m_viewManager;
+        public void CreateLobby()
+        {
+            string lobbyName = m_lobbyName.text;
+            if (lobbyName == "") lobbyName = "New Lobby";
+            m_lobbyManager.CreateRoom(lobbyName);
+            m_viewManager.OnRoomCreateClicked();
+        }
+    }
+}
--- Assets/PurrLobby/Runtime/ViewManagement/Views/LobbyView.cs
+++ Assets/PurrLobby/Runtime/ViewManagement/Views/LobbyView.cs
@@ -5,11 +5,13 @@ namespace PurrLobby
     public class LobbyView : View
     {
         [SerializeField] private CodeButton codeButton;
+        [SerializeField] private LobbyNameButton lobbyButton;
         [SerializeField] private LobbyManager lobbyManager;
 
         public override void OnShow()
         {
             codeButton.Init(lobbyManager.CurrentLobby.LobbyId);
+            lobbyButton.Init(lobbyManager.CurrentLobby.Name);
         }
     }
 }
