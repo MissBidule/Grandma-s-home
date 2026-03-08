a9b206d6565dac20363034cc6c4649df7c17ec8b
karimasoul <105887371+karimasoul@users.noreply.github.com>
2026-03-08 22:55:09 +0100

    it's working!
    But I have to clean my mess....

--- Assets/Script/Managers/ScoreManager.cs
+++ Assets/Script/Managers/ScoreManager.cs
@@ -1,3 +1,4 @@
+using System.Linq.Expressions;
 using PurrNet;
 using TMPro;
 using UnityEngine;
@@ -10,13 +11,13 @@ public class ScoreManager : NetworkBehaviour
 {
 
     [SerializeField] private SyncDictionary<PlayerID, ScoreData> scores = new();
-    [SerializeField] private SyncDictionary<PlayerID, ScoreData> scoresSecNotRes = new(); // il sert a rien pour l'instant
+    //[SerializeField] private SyncDictionary<PlayerID, ScoreData> scoresSecNotRes = new(); // il sert a rien pour l'instant
     //[SerializeField] private SyncDictionary<PlayerID, ScoreData> scoresfinal = new();
     [SerializeField] private TMP_Text m_scoreText;
     //[SerializeField] private float m_scoreSabotageFinal=0;
     [SerializeField] private float m_scoreSabotage;
     [SerializeField] private int m_scoreBroken;
-    [SerializeField] private float sabotageBonusTotal = 0f; // point timer
+    [SerializeField] private SyncVar<float> sabotageBonusTotal = new(); // point timer
     private float timer;
 
 
@@ -27,6 +28,7 @@ public class ScoreManager : NetworkBehaviour
 
     private void Update()
     {
+        RefreshUI();
         if (!isServer) return;
 
         timer += Time.deltaTime;
@@ -37,7 +39,7 @@ public class ScoreManager : NetworkBehaviour
             AddSabotageBonus();
         }
         //SyncPoint();
-        RefreshUI();
+        
     }
 
 
@@ -61,22 +63,23 @@ public class ScoreManager : NetworkBehaviour
             totalSabotage += entry.Value.pointSabotage;
         }
 
-        sabotageBonusTotal += totalSabotage * 0.3f;
+        sabotageBonusTotal.value += totalSabotage * 0.3f; // ca marche sur tous 
     }
     private float GetFinalScore() //c le return qui est important
     {
         float sabotagePoints = 0;
-
         foreach (var entry in scores)
         {
+            Debug.Log("Player sabotage: " + entry.Value.pointSabotage);
             sabotagePoints += entry.Value.pointSabotage;
         }
-
-        return sabotagePoints + sabotageBonusTotal;
+        Debug.Log("SabotageSum = " + sabotagePoints);
+        Debug.Log("Bonus = " + sabotageBonusTotal.value);
+        return sabotagePoints + sabotageBonusTotal.value;
     }
 
     [ServerRpc(requireOwnership:false)]
-    public void AddPointSabotage(PlayerID playerID)
+    public void AddPointSabotage(PlayerID playerID) // ca marche sur tous
     {
         CheckForDictonaryEntry(playerID);
 
@@ -84,9 +87,9 @@ public class ScoreManager : NetworkBehaviour
         ScoreData.pointSabotage++;
         scores[playerID] = ScoreData; 
 
-        var ScoreData2 = scoresSecNotRes[playerID];
-        ScoreData2.pointSabotage++;
-        scoresSecNotRes[playerID] = ScoreData2;
+       // var ScoreData2 = scoresSecNotRes[playerID];
+        //ScoreData2.pointSabotage++;
+        //scoresSecNotRes[playerID] = ScoreData2;
          
     }
 
@@ -163,23 +166,23 @@ public class ScoreManager : NetworkBehaviour
            // la liste 2 fait la list 1 *0.03f tout les x sec 
            // 
 // arreter le chronos
-    public void SyncPoint() //telemment merdique comme fonction
-    {
-        m_scoreSabotage=0;
-        foreach (var entry in scores)
-        {
-            m_scoreSabotage += entry.Value.pointSabotage;
+    //public void SyncPoint() //telemment merdique comme fonction
+    //{
+        //m_scoreSabotage=0;
+        //foreach (var entry in scores)
+        //{
+          //  m_scoreSabotage += entry.Value.pointSabotage;
             //foreach (var entry2 in scoresSecNotRes)
             //{
               //  m_scoreSabotage += entry2.Value.pointSabotage;//*0.03f;  // c nul ca choque et decu
             //}
-        }
-        m_scoreBroken=0;
-        foreach (var entry in scores)
-        {
-            m_scoreBroken += entry.Value.pointBroken;
-        }
-    }
+        //}
+        //m_scoreBroken=0;
+        //foreach (var entry in scores)
+       // {
+      //      m_scoreBroken += entry.Value.pointBroken;
+     //   }
+   // }
 
 // faudra rajouter les differents trucs
     private void RefreshUI()
