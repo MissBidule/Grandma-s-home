dd3d3841cfde9d16d77aeeb67a8c65bff49fa9c1
Lilou Alidor <lilou.alidor@gmail.com>
2026-03-10 15:49:35 +0100

    annoying debug

--- Assets/Prefab/SoloPlaceholder/SoloPlayerController.cs
+++ Assets/Prefab/SoloPlaceholder/SoloPlayerController.cs
@@ -35,27 +35,29 @@ public class SoloPlayerController : MonoBehaviour
         verticalInput = Input.GetAxis("Vertical"); 
         if (_controller.isGrounded) 
         {
-        Transform cam = m_playerCamera.transform;
+            Transform cam = m_playerCamera.transform;
 
-        Vector3 forward = cam.forward;
-        Vector3 right = cam.right;
+            Vector3 forward = cam.forward;
+            Vector3 right = cam.right;
 
-        forward.y = 0f;
-        right.y = 0f;
+            forward.y = 0f;
+            right.y = 0f;
 
-        forward.Normalize();
-        right.Normalize();
+            forward.Normalize();
+            right.Normalize();
 
-        Vector3 wishDir;
-        wishDir = (forward * verticalInput).normalized;
-        // must be on a surface to jump so no double-jumping and no movement changes while airborne but make sure Min Move
-        // Distance on the CharacterController is set to e
-        moveVelocity = forward *_moveSpeed * verticalInput; // forward/backward - Z axis
-        if (Input.GetKeyDown(KeyCode.Space)) // space bar
-        {
-            moveVelocity.y = _jumpSpeed; // set upward velocity burst
-        }
-        Quaternion targetRotation = Quaternion.LookRotation(wishDir, Vector3.up);
+            Vector3 wishDir;
+            wishDir = (forward * verticalInput).normalized;
+            // must be on a surface to jump so no double-jumping and no movement changes while airborne but make sure Min Move
+            // Distance on the CharacterController is set to e
+            moveVelocity = forward *_moveSpeed * verticalInput; // forward/backward - Z axis
+            if (Input.GetKeyDown(KeyCode.Space)) // space bar
+            {
+                moveVelocity.y = _jumpSpeed; // set upward velocity burst
+            }
+            if (wishDir.sqrMagnitude > 0.0001f)
+            {
+                Quaternion targetRotation = Quaternion.LookRotation(wishDir, Vector3.up);
 
                 m_rigidbody.MoveRotation(
                     Quaternion.Slerp(
@@ -64,6 +66,7 @@ public class SoloPlayerController : MonoBehaviour
                         _rotationSpeed * Time.fixedDeltaTime
                     )
                 );
+            }
         }
 
         moveVelocity.y += _gravity * Time.deltaTime; // account for gravity
