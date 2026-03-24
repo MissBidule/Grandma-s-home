55a200f18d998d842897d95c6d1d1f7afd4281f2
SkyMind974 <greniersteven97410@gmail.com>
2026-03-24 16:54:06 +0100

    fix description porte à confusion

--- Assets/Script/States/RoundRunningState.cs
+++ Assets/Script/States/RoundRunningState.cs
@@ -17,7 +17,7 @@ namespace Script.States
         [Header("Round Settings")]
         [SerializeField] [Tooltip("Duration of the round in minutes")] private float m_roundDuration;
         [SerializeField] [Tooltip("Number of time the sky will move in the round.")] private int m_sunIncrementNumber = 12;
-        [SerializeField] [Tooltip("DOOR opening duration")] private float m_startDelay = 10f;
+        [SerializeField] [Tooltip("Start duration")] private float m_startDelay = 10f;
 
         [Header("Door")]
         [SerializeField] [Tooltip("The DOOR")] private StartingDoor m_startingDoor;
