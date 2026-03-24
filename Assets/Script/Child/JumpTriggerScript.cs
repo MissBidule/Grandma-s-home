5b6e60371211b0f6f2307e4c73401f2568e1d889
Antoine Vuillet <121035207+Antoine-Vuillet@users.noreply.github.com>
2026-03-24 13:06:56 +0100

    fix: bigger jump hitbox, comment for Janputorigā

--- Assets/Script/Child/JumpTriggerScript.cs
+++ Assets/Script/Child/JumpTriggerScript.cs
@@ -2,6 +2,10 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 
+/*
+ * @brief Contains class declaration for JumpTriggerScript
+ * @detail This script manages a list of colliders that enter and exit a trigger zone, which can be used to determine when a player or object is within a certain area for jumping mechanics.
+ */
 public class JumpTriggerScript : MonoBehaviour
 {
     public List<Collider> m_colliders = new List<Collider>();
