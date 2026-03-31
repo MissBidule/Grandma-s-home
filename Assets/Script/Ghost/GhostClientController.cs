5447ce37be8f7364a268285e39fcb798313d6a9c
Lilou Alidor <lilou.alidor@gmail.com>
2026-03-31 15:21:03 +0200

    Fix stairs

--- Assets/Script/Ghost/GhostClientController.cs
+++ Assets/Script/Ghost/GhostClientController.cs
@@ -274,7 +274,7 @@ public class GhostClientController : NetworkBehaviour
                 m_ghostController.callAnimationCrossFade("ghost_idle", 0.2f);
                 m_isMoving = false;
             }
-        }else
+        } else
         {
             if (!m_isMoving)
             {
