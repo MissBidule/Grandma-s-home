13835ebdbfcc6482f2fcd31b2d7e1ddea0de6c8f
VillerotJustin <62113284+VillerotJustin@users.noreply.github.com>
2026-03-31 10:01:29 +0200

    Vent + VentExit + Linking
    Issue vent roll back position for clients.

---
+++ Assets/Script/HouseBuilding/VentExit.cs
@@ -0,0 +1,24 @@
+using PurrNet.Logging;
+using UnityEngine;
+
+namespace Script.HouseBuilding
+{
+    public class VentExit : MonoBehaviour
+    {
+        [SerializeField] [Tooltip("Where the ghost appear on exit")] private Transform m_exitTransform;
+
+        private void Awake()
+        {
+            if (m_exitTransform == null)
+                PurrLogger.LogWarning($"{name} exit transform is null", this);
+        }
+        
+        public Transform GetExitTransform
+        {
+            get
+            {
+                return m_exitTransform;
+            }
+        }
+    }
+}
