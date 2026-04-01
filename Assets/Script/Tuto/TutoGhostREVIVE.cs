9c66429bc1711c039e0a6ae863c0d9d48af8c9e9
karimasoul <105887371+karimasoul@users.noreply.github.com>
2026-04-01 16:08:22 +0200

    avance

--- Assets/Script/Tuto/TutoGhostREVIVE.cs
+++ Assets/Script/Tuto/TutoGhostREVIVE.cs
@@ -4,14 +4,15 @@ public class TutoGhostREVIVE : MonoBehaviour
 {
     public ReviveBarUI m_reviveBarUI;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        m_reviveBarUI.Show();
-    }
 
     // Update is called once per frame
     void Update()
     {
         m_reviveBarUI.SetProgress(1f); m_reviveBarUI.Show();
+    }
+    void Awake()
+    {
+        m_reviveBarUI.Show();
+        Debug.Log("why");
     }
 }
