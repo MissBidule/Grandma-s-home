b63d062d4497eec4a0cf127ddf2ab2d9babb000d
karimasoul <105887371+karimasoul@users.noreply.github.com>
2026-04-10 15:03:47 +0200

    avance (need to fix asap)

---
+++ Assets/Script/Tuto/TutoPlayerSpawningState.cs
@@ -0,0 +1,103 @@
+using PurrNet;
+using System.Collections.Generic;
+using PurrNet.StateMachine;
+using UnityEngine;
+using PurrNet.Modules;
+using PurrLobby;
+using System;
+using PurrNet.Transports;
+using Script.UI.Views;
+using UI;
+using Antony;
+using System.Linq;
+namespace Script.States
+{
+    /*
+     * @brief  Contains class declaration for the state PlayerSpawningState
+     * @details Script that will handle the correct spawning of each player element
+     */
+    public class TutoPlayerSpawningState : StateNode
+    {
+        [Header("Child spawner")]
+        [SerializeField] private ChildController m_childPrefab;
+        [SerializeField] private List<Transform> m_childSpawnPoints = new List<Transform>();
+
+        [Header("Ghost spawner")]
+        [SerializeField] private GhostController m_ghostPrefab;
+        [Tooltip("Even if rules are to not despawn on disconnect, this will ignore that and always spawn a player.")]
+        [SerializeField] private List<Transform> m_ghostSpawnPoints = new List<Transform>();
+        private bool m_isServer = false;
+        private bool m_hasStarted = false;
+
+        public override void Enter(bool _asServer)
+        {
+            base.Enter(_asServer);
+
+            m_isServer = _asServer;
+            m_hasStarted = false;
+        }
+
+        public void StartMachine()
+        {
+            if (!m_isServer) return;
+            if (m_hasStarted) return;
+            m_hasStarted = true;
+
+            DespawnPlayers();
+
+            List<PlayerControllerCore> spawnedPlayers = SpawnPlayers();
+
+            // We still keep the player list in case for future implementation of round running state.
+            Debug.Log($"{spawnedPlayers.Count} Player spawned moving to next state.");
+            machine.Next(spawnedPlayers);
+        }
+
+        private List<PlayerControllerCore> SpawnPlayers()
+        {
+            List<PlayerControllerCore> spawnedPlayers = new List<PlayerControllerCore>();
+            RoleKeeper roleKeeper = FindAnyObjectByType<RoleKeeper>();
+
+            int currentSpawnChildIndex = 0;
+            int currentSpawnGhostIndex = 0;
+            foreach (var player in networkManager.players)
+            {
+                if (NetworkManager.main.TryGetModule(out GlobalOwnershipModule ownership, true) && ownership.PlayerOwnsSomething(player))
+                    continue;
+
+                //CONNECTION
+                networkManager.GetModule<PlayersManager>(m_isServer).TryGetConnection(player, out Connection conn);
+
+                bool isGhost = roleKeeper.IsGhost(conn.connectionId);
+
+                Transform spawnPoint,spawnPoint2;
+                PlayerControllerCore newPlayer, newPlayer2;
+
+                spawnPoint = m_ghostSpawnPoints[0];
+                newPlayer = UnityProxy.Instantiate(m_ghostPrefab, spawnPoint.position, spawnPoint.rotation);
+
+                spawnPoint2 = m_childSpawnPoints[0];
+                newPlayer2 = UnityProxy.Instantiate(m_childPrefab, spawnPoint2.position, spawnPoint2.rotation);
+
+                
+                newPlayer.GiveOwnership(player);
+                newPlayer2.GiveOwnership(player);
+
+                spawnedPlayers.Add(newPlayer);
+                spawnedPlayers.Add(newPlayer2);
+            }
+
+            return spawnedPlayers;
+        }
+
+        private void DespawnPlayers()
+        {
+            PlayerControllerCore[] allPlayers = FindObjectsByType<PlayerControllerCore>(FindObjectsInactive.Exclude, FindObjectsSortMode.None);
+
+            foreach (PlayerControllerCore player in allPlayers)
+            {
+                Destroy(player.gameObject);
+            }
+        }
+
+    }
+}
