6b541987f3f1d50afc5355aeaf74ec4feee299e0
karimasoul <105887371+karimasoul@users.noreply.github.com>
2026-04-10 15:15:31 +0200

    avance

--- Assets/Script/Tuto/TutoInstructions.cs
+++ Assets/Script/Tuto/TutoInstructions.cs
@@ -4,7 +4,7 @@
 using UnityEngine;
 using TMPro;
 
-public class TutoManager : MonoBehaviour
+public class TutoInstructions : MonoBehaviour
 {
     [Header("Prefab UI")]
     [SerializeField] private GameObject m_canvasPrefab;
