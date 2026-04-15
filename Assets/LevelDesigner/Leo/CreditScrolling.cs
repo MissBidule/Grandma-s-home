0aebe60a236a5145e6011afebad360c80388ad7a
Léomgn <146841577+leomougin@users.noreply.github.com>
2026-04-15 13:53:01 +0200

    ajout crédit scrolling

---
+++ Assets/LevelDesigner/Leo/CreditScrolling.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public float scrollSpeed = 2f;
+    private bool isScrolling = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+    public void StartScrolling()
+    {
+        Debug.Log("StartScrolling appelé !");
+        isScrolling = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isScrolling) return;
+        Debug.Log("Position : " + transform.position);
+        transform.position += new Vector3(0, 0, scrollSpeed + Time.deltaTime);
+    }
+}
