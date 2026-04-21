c6a0d42ba4b5865713b51cb05b9b1a63d096674f
John LI <128924487+TMTVato@users.noreply.github.com>
2026-04-21 17:20:08 +0200

    fix settings menu crop and size

--- Assets/LevelDesigner/John/scene_john_menu/Menu Ingame script/TabColorChanger.cs
+++ Assets/LevelDesigner/John/scene_john_menu/Menu Ingame script/TabColorChanger.cs
@@ -5,27 +5,34 @@ public class SetDefaultTabColor : MonoBehaviour
 {
     [Header("Couleurs")]
     public Color activeColor = new Color(1f, 0.6f, 0f); // Orange
-    public Color normalColor = Color.white;            // Blanc/Gris
+    public Color normalColor = Color.white;            // Blanc
 
     [Header("Assigner les objets")]
     public Image tabImage;
     public Toggle tabToggle;
+    
+    // AJOUT : Le ScrollRect à remonter
+    public ScrollRect associatedScrollRect; 
 
     void Start()
     {
         if (tabToggle != null && tabImage != null)
         {
-            // 1. On règle la couleur tout de suite au démarrage
             UpdateVisual(tabToggle.isOn);
-
-            // 2. On dit au Toggle d'appeler "UpdateVisual" à chaque fois qu'on clique
             tabToggle.onValueChanged.AddListener(UpdateVisual);
         }
     }
 
-    // Cette fonction change la couleur selon l'état (vrai ou faux)
     void UpdateVisual(bool isOn)
     {
-        tabImage.color = isOn ? activeColor : normalColor;
+        // 1. Change la couleur
+        //tabImage.color = isOn ? activeColor : normalColor;
+
+        // 2. AJOUT : Si on active l'onglet, on reset le scroll en haut
+        if (isOn && associatedScrollRect != null)
+        {
+            associatedScrollRect.verticalNormalizedPosition = 1f;
+            Debug.Log("Scroll Reset pour : " + gameObject.name);
+        }
     }
 }
