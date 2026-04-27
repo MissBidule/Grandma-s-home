b9da546fa8a4cb41ee10ae504dc1d9dc1242c6ed
XadosIV <83866570+XadosIV@users.noreply.github.com>
2026-04-27 14:22:27 +0200

    upsi, forgot that preview can rotate

--- Assets/Script/Ghost/GhostMorph.cs
+++ Assets/Script/Ghost/GhostMorph.cs
@@ -57,9 +57,7 @@ public class GhostMorph : NetworkBehaviour
         m_currentPrefab.GetComponent<MeshCollider>().convex = true;
         m_currentPrefab.transform.localPosition = _position;
         m_currentPrefab.transform.localScale = _prefab.transform.lossyScale;
-        // Account for parent rotation when setting local rotation
-        Quaternion parentRotation = _prefab.transform.parent != null ? _prefab.transform.parent.rotation : Quaternion.identity;
-        m_currentPrefab.transform.localRotation = Quaternion.Inverse(parentRotation) * _rotation;
+        m_currentPrefab.transform.localRotation = _rotation;
         m_isMorphed = true;
     }
 
