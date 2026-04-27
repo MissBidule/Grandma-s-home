18e7c693efa50213b60ed0be1f932e0f6a9f5400
Lilou Alidor <lilou.alidor@gmail.com>
2026-04-27 13:48:53 +0200

    added da balls but not the y

---
+++ Assets/Resources/ballsy/Dangan.cs
@@ -0,0 +1,22 @@
+/**
+ * @brief  This function allows a Slime Decal to appear upon impact before the ball is destroyed.
+ * 
+ * When the player fires a ball, a Slime Decal appears at the point of impact under certain conditions (only one Slime Decal is allowed at a time).
+ * 
+ * @param  m_lifeTime:  lifespan of the ball before disappearance
+ * @param  m_currentLife:  current life left
+ * @param  m_speed:  ball speed
+ * @param  m_offsetFromSurface:  offset added
+ * @param  m_slimePrefab:  Decal Prefab
+ * @param  m_slimeOnCollider:  A dictionary that stores whether a collider already contains a Decal
+ */
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using PurrNet;
+
+public class Dangan : Bullet
+{
+    [Header("Sound")]
+    [SerializeField] AudioClip m_audioClip = null;
+}
